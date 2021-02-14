fa7487532a20f936dcf2393d011bdbcdd38cc409
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2021-02-14 17:08:16 +0100

    blog-static-content

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,47 +1,57 @@
 <template>
-  <article>
-    <h1>{{ mountain.title }}</h1>
-    <p v-if="$fetchState.pending">
-      <span class="loading"></span>
-    </p>
-    <p v-else-if="$fetchState.error">Error while fetching mountains 🤬</p>
-    <section>
-      <!-- <img :src="mountain.image" :alt="mountain.title" /> -->
-      <p>{{ mountain.description }}
-<!-- {{description}} -->
-
-      </p>
+  <article> <section>
+      {{ blog.slug }}
+   
     </section>
-    <button @click="goBack">Back</button>
+    <!-- <button @click="goBack">Back</button>{{ blog.slug }} -->
   </article>
 </template>
 
 
 <script>
 export default {
-    watch: {
-    '$route.query': '$fetch'
+
+ async asyncData({ $content, params }) {
+    // fetch our article here
+    const blog = await $content("blogs", params.slug).fetch();
+
+    return { blog };
   },
-  data() {
-    return {
-    //   mountain: this.$route.params.slug,
-       mountain: {}
-    }
-  },
-  async fetch() {
-    this.mountain = await this.$http.$get(
-    //   `http://localhost:3000/blog.json?this.$route.params.slug`
-    //   `http://localhost:3000/blog.json?${this.$route.params.slug}`
-      `https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m`
-    )
-  },
-  methods: {
-    goBack() {
-      return this.$router.go(-1)
-    }
-  }
+
+
 }
+
+
+
+// export default {
+//     watch: {
+//     '$slug.query': '$fetch'
+//   },
+//   data() {
+//     return {
+//     //   mountain: this.$route.params.slug,
+//        blog: {}
+//     }
+//   },
+//   async fetch() {
+//     this.blog = await this.$http.$get(
+//     //   `http://localhost:3000/blog.json?this.$route.params.slug`
+//     //   `http://localhost:3000/blog.json?${this.$route.params.slug}`
+//     //   `https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m&${this.$route.params.slug}`  
+//     `https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m`
+//     )
+//   },
+//   methods: {
+//     goBack() {
+//       return this.$router.go(-1)
+//     }
+//   }
+// }
 </script>
+
+
+
+
 <style scoped>
 article {
   max-width: 600px;
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,15 @@
 <template>
   
 <div>
-
-<the-blog-all/>
+ <ul class="max-w-4xl mt-8">
+                        <li v-for="blog of blogs" :key="blog.slug" class="py-4 pl-4 my-4 bg-white border border-gray-500 border-opacity-25 shadow-md rounded-xl">
+                             <NuxtLink :to="{ name: 'blog-slug', params: { slug: blog.slug } }"> 
+                                <p>{{ blog.slug }}</p>
+                                <p>{{ blog.title }}</p>
+                            </NuxtLink> 
+                        </li>
+                  </ul>
+<!-- <the-blog-all/> -->
 
 </div>
 
@@ -21,20 +28,31 @@
 </template>
 
 <script>
-import TheBlogAll from '../../components/TheBlogAll.vue'
+
   export default {
-  components: { TheBlogAll },
-    data() {
-      return {
-        mountains: []
-      }
-    },
-    async fetch() {
-      this.mountains = await fetch(
-        'https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m'
-      ).then(res => res.json())
-    }
+ async asyncData({ $content, params }) {
+    // fetch our article here
+    const blogs = await $content("blogs", params.slug).fetch();
+
+    return { blogs };
   }
+  }
+
+// import TheBlogAll from '../../components/TheBlogAll.vue'
+//   export default {
+//   components: { TheBlogAll },
+//     data() {
+//       return {
+//         mountains: []
+//       }
+//     },
+//     async fetch() {
+//       this.mountains = await fetch(
+//         // 'https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m'
+//         'https://script.googleusercontent.com/macros/echo?user_content_key=iJLLGWy2XR3sBUCgLGyOkMvMEtQyBtDX432ju76TaihrsAWR3cGyLpJKIuY6jm6C-mqVEylQlWywuJOpMs7XlCJK27Nw5KP_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEgKUSEKZAbo5b7L4PwFjGqt_woMYrb75_GzgQmLwUbGQbjOPl8Wh2pVXHVJwPC6Bbc5Yu1BFlTCGGaU2pP6Ntk&lib=MUx4MpC2NIHY9zDpjxHlJlBlfZc1fPD1m'
+//       ).then(res => res.json())
+//     }
+//   }
 </script>
 
 <style>
